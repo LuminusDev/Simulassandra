e1d145b3c3677aa959844842cab8004f256539b2
Guillaume Marques <guillaume.marques33@gmail.com>
2015-03-03 15:14:46 +0100

    init commit ClientÃ

---
+++ Client/client/src/main/java/simulassandra/client/App.java
@@ -0,0 +1,17 @@
+package simulassandra.client;
+
+import com.datastax.driver.core.Cluster;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	private Cluster cluster; // fonctionne ok!
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
